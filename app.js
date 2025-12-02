dcce510263babadc5e48d5cc0572fd96d5b4cd73
Pavani-cyber <ppavanipagadala@gmail.com>
2025-12-02 16:17:34 +0530

    updated url

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.engine('ejs', ejsMate);
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname,"/public")));
 
-const store=MongoStore.create({
+const store=MongoStore({
     mongoUrl:dburl,
     crypto:{
         secret:process.env.SECRET,
